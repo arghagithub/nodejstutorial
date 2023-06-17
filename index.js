e56b24e08dc821bc5c3b4476ef0202f6b77e7bad
Argha <arghagolui@gmail.com>
2023-06-17 09:54:41 +0530

    video 5 completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("Welcome to node js");
+
+const fs= require('fs');
+//write to any non existent file
+// fs.writeFileSync('read.txt',"Hello, I am Argha Golui",'utf-8');
+
+//update the file
+
+// fs.writeFileSync('read.txt',"Hello, I am Argha Golui, who are you?",'utf-8');
+
+//append in the file
+
+// fs.appendFileSync('read.txt'," \n John Dahl");
+
+// const data= fs.readFileSync('read.txt','utf-8');
+// console.log(data);
+
+fs.renameSync('read.txt','readwrite.txt');
+
