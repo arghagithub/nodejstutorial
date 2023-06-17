cc2d9c6f21037f015357420e0745237c57d03e56
Argha <arghagolui@gmail.com>
2023-06-17 14:56:22 +0530

    video 11 completed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs= require('fs');
 const os=require('os');
 const path=require('path');
+const oper=require('./script');
+const {add,sub,mul}=oper;
 // fs.writeFile('hello.txt','My name is Argha Golui','utf-8',(err)=>{
 //     if(!err){
 //         console.log("data is written on the file successfully");
@@ -39,8 +41,12 @@ const path=require('path');
 // console.log(os.tmpdir());
 
 
-console.log(path.dirname("E:/bmicalculatormernproject/Backend/middleware/fetchadmin.js"));
-console.log(path.extname("E:/bmicalculatormernproject/Backend/middleware/fetchadmin.js"));
-console.log(path.basename("E:/bmicalculatormernproject/Backend/middleware/fetchadmin.js"));
-const data=path.parse("E:/bmicalculatormernproject/Backend/middleware/fetchadmin.js");
-console.log(data.root);
+// console.log(path.dirname("E:/bmicalculatormernproject/Backend/middleware/fetchadmin.js"));
+// console.log(path.extname("E:/bmicalculatormernproject/Backend/middleware/fetchadmin.js"));
+// console.log(path.basename("E:/bmicalculatormernproject/Backend/middleware/fetchadmin.js"));
+// const data=path.parse("E:/bmicalculatormernproject/Backend/middleware/fetchadmin.js");
+// console.log(data.root);
+
+console.log(add(5,2));
+console.log(sub(5,1));
+console.log(mul(5,2));
