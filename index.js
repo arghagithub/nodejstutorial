2ec08afa7cb306be5ac2c022ed959ef5ac6992c8
Argha <arghagolui@gmail.com>
2023-06-17 14:09:36 +0530

    video 11 completed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs= require('fs');
 const os=require('os');
+const path=require('path');
 // fs.writeFile('hello.txt','My name is Argha Golui','utf-8',(err)=>{
 //     if(!err){
 //         console.log("data is written on the file successfully");
@@ -30,9 +31,16 @@ const os=require('os');
 // console.log("Asynchorus javascript");
 
 
-console.log(os.type());
-console.log(os.arch());
-console.log(os.platform());
-console.log(`${os.totalmem()/1024/1024/1024}`);
-console.log(`${os.freemem()/1024/1024/1024}`);
-console.log(os.tmpdir());
+// console.log(os.type());
+// console.log(os.arch());
+// console.log(os.platform());
+// console.log(`${os.totalmem()/1024/1024/1024}`);
+// console.log(`${os.freemem()/1024/1024/1024}`);
+// console.log(os.tmpdir());
+
+
+console.log(path.dirname("E:/bmicalculatormernproject/Backend/middleware/fetchadmin.js"));
+console.log(path.extname("E:/bmicalculatormernproject/Backend/middleware/fetchadmin.js"));
+console.log(path.basename("E:/bmicalculatormernproject/Backend/middleware/fetchadmin.js"));
+const data=path.parse("E:/bmicalculatormernproject/Backend/middleware/fetchadmin.js");
+console.log(data.root);
