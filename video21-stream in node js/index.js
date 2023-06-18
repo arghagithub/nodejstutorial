b111625628c1eae0313fbabb8b911dacdc88d2d3
Argha <arghagolui@gmail.com>
2023-06-18 14:54:40 +0530

    video 22: pipe method is completed

--- video21-stream in node js/index.js
+++ video21-stream in node js/index.js
@@ -5,6 +5,7 @@ const server=http.createServer();
 const port=80;
 
 server.on('request',(req,res)=>{
+    //1st way
     // fs.readFile('input.txt','utf-8',(err,data)=>{
     //     if(!err){
     //         res.end(data);
@@ -13,17 +14,28 @@ server.on('request',(req,res)=>{
     //         res.end('<h1>404 bad request</h1>');
     //     }
     // })
+
+
+
+    //2nd way
+    // const rstream=fs.createReadStream('input.txt');
+    // rstream.on('data',(chunkdata)=>{
+    //     res.write(chunkdata);
+    // })
+    // rstream.on('end',()=>{
+    //     res.end();
+    // })
+    // rstream.on('error',(err)=>{
+    //     res.end("page not found")
+    //     console.log(err);
+    // })
+
+
+
+    //3rd way
     const rstream=fs.createReadStream('input.txt');
-    rstream.on('data',(chunkdata)=>{
-        res.write(chunkdata);
-    })
-    rstream.on('end',()=>{
-        res.end();
-    })
-    rstream.on('error',(err)=>{
-        res.end("page not found")
-        console.log(err);
-    })
+    rstream.pipe(res);//je speed e read korbe, sei speed e dhana dhan write korbe
+
 })
 
 server.listen(port,'127.0.0.1',()=>{
